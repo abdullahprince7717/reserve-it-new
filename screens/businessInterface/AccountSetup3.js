8a0e298bbe80daf4545e78962e7e173e99a47423
abdullahprince7717 <abdullahprince7717@gmail.com>
2023-05-19 04:01:40 +0500

    maps in business account setup done

--- screens/businessInterface/AccountSetup3.js
+++ screens/businessInterface/AccountSetup3.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { useRef, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { useRef, useState, useEffect } from 'react';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { collection, doc, addDoc, getDocs, setDoc } from "firebase/firestore";
+import { db, auth, storage } from '../../firebase/FirebaseConfig.js'
+import { Button } from 'react-native-paper';
 
-export default function App() {
+export default function AccountSetup3(props) {
     const [count, setCount] = useState(0);
     const [draggableMarkerCoord, setDraggableMarkerCoord] = useState({
         latitude: 31.4027,
-        // latitudeDelta: 0.0043,
         longitude: 74.2126,
-        // longitudeDelta: 0.0034,
+
     });
     const mapRef = useRef();
+    const docRef = doc(db, "business_users", auth.currentUser.uid);
 
     const onRegionChange = (region) => {
     };
+    const addLocationInfo = async () => {
+
+
+
+
+        await setDoc(docRef, draggableMarkerCoord
+            , { merge: true })
+            .then(
+                (res) => {
+                    console.log("response" + res)
+
+                })
+            .catch(
+                (err) => {
+                    console.log("error" + err)
+                });
+    };
+
+    useEffect(() => {
+        addLocationInfo();
+    })
 
 
 
@@ -39,9 +62,33 @@ export default function App() {
                     pinColor='#0000ff'
                     coordinate={draggableMarkerCoord}
                     onDragStart={(e) => setDraggableMarkerCoord(e.nativeEvent.coordinate)}
-                    onDragEnd={(e) => setDraggableMarkerCoord(e.nativeEvent.coordinate)}
+                    onDragEnd={(e) => {
+                        setDraggableMarkerCoord(e.nativeEvent.coordinate)
+
+                    }}
                 />
             </MapView>
+
+            <View
+                style={{
+                    position: 'absolute', top: '85%', alignSelf: 'flex-end', left: '14%',
+                }}
+            >
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                        console.log("Pressed SAVE")
+                        addLocationInfo();
+                        props.navigation.navigate('AccountSetup4')
+                    }}
+                >
+
+                    <Text style={{ color: '#fff', fontSize: 17 }}>
+                        NEXT
+                    </Text>
+
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -57,15 +104,13 @@ const styles = StyleSheet.create({
         width: '100%',
         height: '100%'
     },
-    mapOverlay: {
-        position: "absolute",
-        bottom: 50,
-        backgroundColor: "#ffffff",
-        borderWidth: 2,
-        borderRadius: 5,
-        padding: 16,
-        left: "25%",
-        width: "50%",
-        textAlign: "center"
+    button: {
+        backgroundColor: '#57B9BB',
+        width: '110%',
+        height: 50,
+        borderRadius: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
     }
 });
